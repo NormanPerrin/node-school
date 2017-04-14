abe15bce64be4f411212fd543f0582c94f1a81dd
Norman <norman.perrin.94@gmail.com>
2017-04-14 00:15:35 -0300

    Add electron lesson

---
+++ elementary-electron/cat-annotation/index.js
@@ -0,0 +1,7 @@
+var picture = require('cat-picture');
+var src = picture.src;
+picture.remove();
+
+var image = require('lightning-image-poly');
+new image('#visualization', null, [src], {hullAlgorithm: 'convex'});
+
